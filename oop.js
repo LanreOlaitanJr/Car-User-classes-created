5e5bc57eec375fec4776fd60ee146a6af6db1d90
LanreOlaitanJr <olaitanjr25@gmail.com>
2024-08-12 11:02:03 +0100

    Abstraction

--- oop.js
+++ oop.js
@@ -1,3 +1,4 @@
+// CLASSES
 class Car {
     constructor(name, model, year){
         this.name = name
@@ -10,7 +11,7 @@ class Car {
     }
 }
 
-class user {
+class User {
     constructor(fullname, email, phone, password){
         this.fullname = fullname
         this.email = email
@@ -25,3 +26,19 @@ class user {
         return(`${this.fullname} has logged out successfully`)
     }
 }
+
+// ABSTRACTION
+const myCar = new Car("Tesla", "x-6", 2024)
+console.log(myCar);
+
+const hisCar = new Car("BMW", "x-8", 2026)
+console.log(hisCar);
+
+const user = new User("Olaitan", "ola@gmail.com", 8076765456, "12345678")
+console.log(user);
+
+console.log(myCar.displayInfo());
+console.log(user.login());
+console.log(user.logout());
+
+
